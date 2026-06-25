39c7f640326849e908b6bdc74a4b21df90ca0fb0
paginainsta32-bit <paginainsta32@gmail.com>
2026-06-25 09:40:37 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+﻿async function login(){
+
+    const usuario =
+        document.getElementById("usuario").value;
+
+    const senha =
+        document.getElementById("senha").value;
+
+    try{
+
+        const response = await fetch(
+            `${API_URL}/${DATABASE.usuarios}/?user_field_names=true`,
+            {
+                headers:{
+                    Authorization:`Token ${BASEROW_TOKEN}`
+                }
+            }
+        );
+
+        const data = await response.json();
+
+        const usuarioEncontrado =
+            data.results.find(u =>
+                u.usuario === usuario &&
+                u.senha === senha
+            );
+
+        if(usuarioEncontrado){
+
+            localStorage.setItem(
+                "usuarioLogado",
+                usuario
+            );
+
+            alert("Login realizado!");
+
+            window.location =
+                "pages/dashboard.html";
+
+        }else{
+
+            alert("Usuário ou senha inválidos");
+
+        }
+
+    }catch(error){
+
+        console.error(error);
+
+        alert("Erro ao conectar");
+
+    }
+
+}
---
+++ config.js
@@ -0,0 +1,11 @@
+﻿const BASEROW_TOKEN = "SEU_NOVO_TOKEN";
+
+const DATABASE = {
+    usuarios: 1045685,
+    produtos: 1045702,
+    pedidos: 1045705,
+    itensPedido: 1045708,
+    despesas: 1045710
+};
+
+const API_URL = "https://api.baserow.io/api/database/rows/table";
